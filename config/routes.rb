cd1ba6ab0207bef4d790c8f8976cba64af33226d
tanderegg <timothy.anderegg@gmail.com>
2010-09-07 19:02:59 -0400

    Finished basic AuthLogic implementation, worked on graphic design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
+	map.login "login", :controller => "user_sessions", :action => "new"
+	map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
+  map.resources :user_sessions
+  map.resources :users
+  
+  map.root :controller => "users", :action => "index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
